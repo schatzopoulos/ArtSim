5e256f29dd591423928627de73d71cf2458acf88
Serafeim Chatzopoulos <s.chatzopoulos@gmail.com>
2021-01-09 16:54:37 +0200

    Refactoring: kendall's tau as function

--- src/artsim-core.py
+++ src/artsim-core.py
@@ -1,11 +1,11 @@
 import sys
 import pprint
 import statistics
-
+from rank_distance import tau
 pp = pprint.PrettyPrinter(indent=4)
 
-if len(sys.argv) != 8:
-    print("Usage: python sim.py <paper_details_file> <scores_file> <sim_file1> <sim_file2> <cold_start_year> <aggr: median|mean> <output_dir>")
+if len(sys.argv) != 10:
+    print("Usage: python sim.py <paper_details_file> <scores_file> <sim_file1> <sim_file2> <cold_start_year> <aggr: median|mean> <output_file> <alpha> <beta>")
     sys.exit(-1)
 
 
@@ -15,7 +15,14 @@ sim_file_PA = sys.argv[3]
 sim_file_PT = sys.argv[4]
 cold_start_year = int(sys.argv[5])
 aggr = sys.argv[6]
-output_dir = sys.argv[7]
+output_file = sys.argv[7]
+alpha = float(sys.argv[8])
+beta = float(sys.argv[9])
+gamma = 1 - alpha - beta
+dblp_fcc = "../data/evaluation/dblp_fcc_varying_future_period_30percent.txt"
+
+
+class ArtSimCore:
 
 # read similarities
 def read_similarties(sim_file, sim_name):
@@ -44,6 +51,7 @@ def read_similarties(sim_file, sim_name):
             line = fp.readline()
 
 # load paper code & ids
+
 paper_ids = {}
 with open(paper_details, encoding="utf8") as fp:
     line = fp.readline()
@@ -76,62 +84,52 @@ with open(scores_file) as fp:
             papers[paper_id]['similar']['PT'] = []
 
             papers_in_cold_start_num += 1
-        
+
         line = fp.readline()
         papers_num += 1
 
-# print(papers)
-# print("TOTAL PAPERS: " + str(papers_num))
-# print("PAPERS IN COLD START: " + str(papers_in_cold_start_num))
 
 read_similarties(sim_file_PA, 'PA')
 read_similarties(sim_file_PT, 'PT')
 
-# weights = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-weights = ((0.2, 0.1, 0.7), (0.3, 0.1, 0.6), (0.6, 0.1, 0.3), (0.7, 0.1, 0.2))
+print (str(alpha) + "\t" + str(beta) + "\t" + str(gamma))
 
-for w in weights:
-    alpha = w[0]
-    beta = w[1]
-    gamma = w[2]
+fw = open(output_file, "w")
 
-    print (str(alpha) + "\t" + str(beta) + "\t" + str(gamma))
+result = []
+for key in papers:
+    if 'similar' in papers[key]: 
+        score = papers[key]['score']
 
-    fw = open(output_dir + str(alpha) + "_" + str(beta) + "_" + str(gamma) + ".csv", "w")
+        sim_score_PA = 0.0
+        if len(papers[key]['similar']['PA']) > 0:
 
-    result = []
-    for key in papers:
-        if 'similar' in papers[key]: 
-            score = papers[key]['score']
+            # calculate score from PA similarities
+            scores_PA = [item[2] for item in papers[key]['similar']['PA']]
 
-            sim_score_PA = 0.0
-            if len(papers[key]['similar']['PA']) > 0:
-                
-                # calculate score from PA similarities
-                scores_PA = [item[2] for item in papers[key]['similar']['PA']]
+            if aggr == 'median':
+                sim_score_PA = statistics.median(scores_PA)
+            else:
+                sim_score_PA = statistics.mean(scores_PA)
 
-                if aggr == 'median':
-                    sim_score_PA = statistics.median(scores_PA)
-                else:
-                    sim_score_PA = statistics.mean(scores_PA)
+        sim_score_PT = 0.0    
+        if  len(papers[key]['similar']['PT']) > 0:
 
-            sim_score_PT = 0.0    
-            if  len(papers[key]['similar']['PT']) > 0:
+            # calculate score from PT similarities
+            scores_PT = [item[2] for item in papers[key]['similar']['PT']]
 
-                # calculate score from PT similarities
-                scores_PT = [item[2] for item in papers[key]['similar']['PT']]
+            if aggr == 'median':
+                sim_score_PT = statistics.median(scores_PT)
+            else:
+                sim_score_PT = statistics.mean(scores_PT)
 
-                if aggr == 'median':
-                    sim_score_PT = statistics.median(scores_PT)
-                else:
-                    sim_score_PT = statistics.mean(scores_PT)
+        score = alpha * sim_score_PA + beta * sim_score_PT + gamma * score
 
-            score = alpha * score + beta * sim_score_PA + gamma * sim_score_PT
+        fw.write(papers[key]['code'] + "\t" + str(score) + "\t" + str(papers[key]['year']) + "\n")
 
-            fw.write(papers[key]['code'] + "\t" + str(score) + "\t" + str(papers[key]['year']) + "\n")
-
-        else:
-            result.append((papers[key]['code'], papers[key]['score'], papers[key]['year']))
-            fw.write(papers[key]['code'] + "\t" + str(papers[key]['score']) + "\t" + str(papers[key]['year']) + "\n")
-        
-    fw.close()
+    else:
+        result.append((papers[key]['code'], papers[key]['score'], papers[key]['year']))
+        fw.write(papers[key]['code'] + "\t" + str(papers[key]['score']) + "\t" + str(papers[key]['year']) + "\n")
+fw.close()
+kendall_tau = tau(dblp_fcc, output_file)
+print(kendall_tau)
