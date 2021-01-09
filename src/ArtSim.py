45d72c2c2f05766126ce2be951dfe4622d7bc409
Στρ (ΕΠ) Χατζόπουλος <s.chatzopoulos@armyweb.local>
2021-01-09 21:06:02 +0200

    Fix ArtSim class variables

--- src/ArtSim.py
+++ src/ArtSim.py
@@ -4,8 +4,8 @@ import statistics
 pp = pprint.PrettyPrinter(indent=4)
 
 class ArtSim:
-    _paper_ids = {}
-    _papers = {}
+    self._paper_ids = {}
+    self._papers = {}
 
     # read similarities
     def read_similarties(self, sim_file, sim_name):
@@ -20,27 +20,27 @@ class ArtSim:
                 dest_id = int(parts[1])
 
                 # keep similarities for those papers inside the training set
-                if src_id in _papers and dest_id in _papers:
+                if src_id in self._papers and dest_id in self._papers:
 
                     # 'similar' field is present only for papers in cold start
-                    if 'similar' in _papers[src_id]:
-                        _papers[src_id]['similar'][sim_name].append((dest_id, float(parts[2]), _papers[dest_id]['score']))
+                    if 'similar' in self._papers[src_id]:
+                        self._papers[src_id]['similar'][sim_name].append((dest_id, float(parts[2]), self._papers[dest_id]['score']))
                         sim_count += 1
 
                     if 'similar' in papers[dest_id]:
-                        _papers[dest_id]['similar'][sim_name].append((src_id, float(parts[2]), _papers[src_id]['score']))
+                        self._papers[dest_id]['similar'][sim_name].append((src_id, float(parts[2]), self._papers[src_id]['score']))
                         sim_count += 1
                 
                 line = fp.readline()
 
     # load paper code & ids
-    def read_paper_ids(self, paper_details):
+    def readself._paper_ids(self, paper_details):
         with open(paper_details, encoding="utf8") as fp:
             line = fp.readline()
             while line:
                 line = line.rstrip()
                 parts = line.split("\t")
-                _paper_ids[parts[1]] = int(parts[0])   
+                self._paper_ids[parts[1]] = int(parts[0])   
                 line = fp.readline()
 
     # read paper scores and publication year
@@ -57,8 +57,8 @@ class ArtSim:
                 parts = line.split("\t")
                 pub_year = int(parts[2])
 
-                paper_id = _paper_ids[parts[0]]
-                _papers[paper_id] = {
+                paper_id = self._paper_ids[parts[0]]
+                self._papers[paper_id] = {
                     'code': parts[0],
                     'score': float(parts[1]), 
                     'year': pub_year
@@ -66,9 +66,9 @@ class ArtSim:
 
                 # TODO: move to init function so as to reuse function
                 if pub_year >= cold_start_year:
-                    _papers[paper_id]['similar'] = {}
-                    _papers[paper_id]['similar']['PA'] = []
-                    _papers[paper_id]['similar']['PT'] = []
+                    self._papers[paper_id]['similar'] = {}
+                    self._papers[paper_id]['similar']['PA'] = []
+                    self._papers[paper_id]['similar']['PT'] = []
 
                     papers_in_cold_start_num += 1
 
@@ -82,15 +82,15 @@ class ArtSim:
 
         fw = open(output_file, "w")
 
-        for key in _papers:
-            if 'similar' in _papers[key]: 
-                score = _papers[key]['score']
+        for key in self._papers:
+            if 'similar' in self._papers[key]: 
+                score = self._papers[key]['score']
 
                 sim_score_PA = 0.0
-                if len(_papers[key]['similar']['PA']) > 0:
+                if len(self._papers[key]['similar']['PA']) > 0:
 
                     # calculate score from PA similarities
-                    scores_PA = [item[2] for item in _papers[key]['similar']['PA']]
+                    scores_PA = [item[2] for item in self._papers[key]['similar']['PA']]
 
                     if aggr == 'median':
                         sim_score_PA = statistics.median(scores_PA)
@@ -98,10 +98,10 @@ class ArtSim:
                         sim_score_PA = statistics.mean(scores_PA)
 
                 sim_score_PT = 0.0    
-                if  len(_papers[key]['similar']['PT']) > 0:
+                if  len(self._papers[key]['similar']['PT']) > 0:
 
                     # calculate score from PT similarities
-                    scores_PT = [item[2] for item in _papers[key]['similar']['PT']]
+                    scores_PT = [item[2] for item in self._papers[key]['similar']['PT']]
 
                     if aggr == 'median':
                         sim_score_PT = statistics.median(scores_PT)
@@ -110,9 +110,9 @@ class ArtSim:
 
                 score = alpha * sim_score_PA + beta * sim_score_PT + gamma * score
 
-                fw.write(_papers[key]['code'] + "\t" + str(score) + "\t" + str(_papers[key]['year']) + "\n")
+                fw.write(self._papers[key]['code'] + "\t" + str(score) + "\t" + str(self._papers[key]['year']) + "\n")
 
             else:
-                fw.write(_papers[key]['code'] + "\t" + str(_papers[key]['score']) + "\t" + str(_papers[key]['year']) + "\n")
+                fw.write(self._papers[key]['code'] + "\t" + str(self._papers[key]['score']) + "\t" + str(self._papers[key]['year']) + "\n")
         fw.close()
         
