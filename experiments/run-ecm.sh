8f2d68a3c0310e4ecc4f52f5a83f7202b50de901
Στρ (ΕΠ) Χατζόπουλος <s.chatzopoulos@armyweb.local>
2021-01-09 22:38:39 +0200

    Fix run-ecm script

--- experiments/run-ecm.sh
+++ experiments/run-ecm.sh
@@ -4,6 +4,6 @@ years=("2007" "2008" "2009" "2010")
 
 for year in "${years[@]}"
 do
-	python3 ../src/main.py ../data/P.csv ../data/rival_scores/timeawarerank_results_file_dblp_full_graph_oldest_50percent.txt_a0.2_c0.4_year2010_mECM.txt ../data/similarities/PAP_similarities.csv  ../data/similarities/PTP_similarities.csv $year mean tmp-results.csv 0.3 0.1
+	python3 ../src/main.py ../data/P.csv ../data/rival_scores/timeawarerank_results_file_dblp_full_graph_oldest_50percent.txt_a0.2_c0.4_year2010_mECM.txt ../data/similarities/PAP_similarities.csv  ../data/similarities/PTP_similarities.csv $year mean tmp-results.csv
 	break
 done
