b189ba8213afe018a91aad718deafe1da3ddb755
Serafeim Chatzopoulos <s.chatzopoulos@gmail.com>
2021-01-11 16:38:46 +0200

    Add simulated annealing

--- main_annealing.py
+++ main_annealing.py
@@ -1,89 +1,59 @@
 import sys
 import numpy as np
 from ArtSim import ArtSim
-# from rank_distance import tau
+from rank_distance import tau
+from scipy import optimize
 
-# dblp_fcc = "../data/evaluation/dblp_fcc_varying_future_period_30percent.txt"
+dblp_fcc = "../data/evaluation/dblp_fcc_varying_future_period_30percent.txt"
 
-# if len(sys.argv) != 8:
-#     print("Usage: python main.py <paper_details_file> <scores_file> <sim_file1> <sim_file2> <cold_start_year> <aggr: median|mean> <output_file>")
-#     sys.exit(-1)
-
-
-# paper_details = sys.argv[1]
-# scores_file = sys.argv[2]
-# sim_file_PA = sys.argv[3]
-# sim_file_PT = sys.argv[4]
-# cold_start_year = int(sys.argv[5])
-# aggr = sys.argv[6]
-# output_file = sys.argv[7]
-
-# artsim = ArtSim()
-
-# artsim.read_paper_ids(paper_details)
-# artsim.read_paper_scores(scores_file)
-
-# artsim.mark_cold_start_papers(cold_start_year)
-
-# artsim.read_similarities(sim_file_PA, 'PA', aggr)
-# artsim.read_similarities(sim_file_PT, 'PT', aggr)
+if len(sys.argv) != 8:
+    print("Usage: python main.py <paper_details_file> <scores_file> <sim_file1> <sim_file2> <cold_start_year> <aggr: median|mean> <output_file>")
+    sys.exit(-1)
 
 
-# for alpha in np.linspace(0, 0.6, 7):
-#         for beta in np.linspace(0, 0.6, 7):
-#                 for gamma in np.linspace(0, 1, 11):
-#                         alpha = round(alpha, 1)
-#                         beta = round(beta, 1)
-#                         gamma = round(gamma, 1)
-#                         sum = alpha + beta + gamma
-#                         if (round(sum, 1) != 1.0):
-#                                 continue
+paper_details = sys.argv[1]
+scores_file = sys.argv[2]
+sim_file_PA = sys.argv[3]
+sim_file_PT = sys.argv[4]
+cold_start_year = int(sys.argv[5])
+aggr = sys.argv[6]
+output_file = sys.argv[7]
 
-#                         artsim.run(alpha, beta, gamma, output_file)
-#                         kendall_tau = tau(dblp_fcc, output_file)
+artsim = ArtSim()
 
-#                         print (str(alpha) + "\t" + str(beta) + "\t" + str(gamma) + "\t" + str(kendall_tau))
+artsim.read_paper_ids(paper_details)
+artsim.read_paper_scores(scores_file)
+
+artsim.mark_cold_start_papers(cold_start_year)
+
+artsim.read_similarities(sim_file_PA, 'PA', aggr)
+artsim.read_similarities(sim_file_PT, 'PT', aggr)
 
 
+def call_artsim(x):
 
-# for alpha in np.linspace(0, 0.6, 61):
-# 	for beta in np.linspace(0, 0.6, 61):
-# 		for gamma in np.linspace(0, 1, 101):
-# 			alpha = round(alpha, 2)
-# 			beta = round(beta, 2)
-# 			gamma = round(gamma, 2)
-# 			sum = alpha + beta + gamma
-# 			if (round(sum, 2) != 1.0):
-# 				continue
+	if np.sum(x) > 1:
+		return 1000
 
-# 			artsim.run(alpha, beta, gamma, output_file)
-# 			kendall_tau = tau(dblp_fcc, output_file)
+	alpha = x[0]
+	beta = x[1]
+	gamma = 1 - alpha - beta
+	call_artsim.count += 1
 
-# 			print (str(alpha) + "\t" + str(beta) + "\t" + str(gamma) + "\t" + str(kendall_tau)
+	artsim.run(alpha, beta, gamma, output_file)
+    kendall_tau = tau(dblp_fcc, output_file)
 
-def demo_func(x): 
-    demo_func.calls += 1
-    return (x[0] ** 2 + (x[1] - 0.05) ** 2)
+    print (str(artisim_count) + "\t" + str(alpha) + "\t" + str(beta) + "\t" + str(gamma) + "\t" + str(kendall_tau))
 
-# %% Do SA
-from libs.scikit_opt.sko.SA import SA
+    return 1 - kendall_tau
 
-demo_func.calls = 0
-sa = SA(func=demo_func, x0=[1, 1], T_max=1, T_min=1e-9, L=300, max_stay_counter=150)
-best_x, best_y = sa.run()
-print('best_x:', best_x, 'best_y', best_y)
+bounds = [(0, 0.6), (0, 0.6)]
 
-# %% Plot the result
-import matplotlib.pyplot as plt
-import pandas as pd
+call_artsim.count = 0
+result = optimize.dual_annealing(call_artsim, bounds, maxiter=500)
+print(result)
 
-plt.plot(pd.DataFrame(sa.best_y_history).cummin(axis=0))
-plt.show()
-
-# %%
-from sko.SA import SAFast
-
-sa_fast = SAFast(func=demo_func, x0=[1, 1], T_max=1, T_min=1e-9, q=0.99, L=300, max_stay_counter=150)
-sa_fast.run()
-print('Fast Simulated Annealing: best_x is ', sa_fast.best_x, 'best_y is ', sa_fast.best_y)
-print("Total function calls " + str(demo_func.calls))
+print("Function calls: ", result['nfev'])
+print("Best x: ", result['x'])
+print("Best y: ", 1 - result['fun'])
+print(call_artsim.count)
